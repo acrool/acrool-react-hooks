2014b7f2689b2af8782572564939541c98d8b13a
imagine10255 <imagine10255@gmail.com>
2024-08-07 11:41:58 +0800

    remove useClickOutSite

--- src/position/index.ts
+++ src/position/index.ts
@@ -1,4 +1,3 @@
-export {default as useClickOutSite} from './useClickOutSite';
 export {default as usePosition} from './usePosition';
 export * from './usePosition';
 export {default as useScrollTop} from './useScrollTop';
