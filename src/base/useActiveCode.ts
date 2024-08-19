aa1366dde665b24572ca6b03b36ebfc102f8c1c1
imagine10255 <imagine10255@gmail.com>
2024-08-19 09:45:42 +0800

    add useActiveCode.checkIsActive

--- src/base/useActiveCode.ts
+++ src/base/useActiveCode.ts
@@ -9,14 +9,25 @@ import {useCallback, useState} from 'react';
 const useActiveCode = <T>(defaultActiveCode) => {
     const [activeCode, setActiveCode] = useState<T>(defaultActiveCode);
 
+    /**
+     * 產生 設定ActiveCode 方法
+     */
     const generatorSetActiveCode = useCallback((code: T) => {
         return () => {
             setActiveCode(code);
         };
     }, []);
 
+    /**
+     * 檢查是否為 Active
+     */
+    const checkIsActive = useCallback((code: T) => {
+        return activeCode === code;
+    }, []);
+
     return {
         activeCode,
+        checkIsActive,
         setActiveCode,
         generatorSetActiveCode,
     };
